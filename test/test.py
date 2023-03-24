780f1b7a81edcaaf1bc83d02ff58bb700deb526e
yu <373914367@qq.com>
2023-03-24 17:26:17 +0800

    knn ac test

--- test/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-input1 = torch.randn(1,256)
-input2 = torch.randn(1,256)
-cos = nn.CosineSimilarity(dim=1, eps=1e-6)
-output = cos(input1, input2)
-print(output)
